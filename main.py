306bb57272ed317bbf0fab3e0cc1a1f44350800f
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-10-26 16:14:33 +0200

    added list

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
+def draw_snake():
+    snake_pxl_loc = 0
+    while snake_pxl_loc <= len(snake_loc_x) + -1:
+        led.plot(snake_loc_x[snake_pxl_loc], snake_loc_y[snake_pxl_loc])
+        snake_pxl_loc += 1
+def update_snake_y():
+    global snake_temp_loc_y
+    snake_temp_loc_y = snake_loc_y[len(snake_loc_x) - 1] + snake_velocity_y
+    if snake_temp_loc_y == 5:
+        snake_temp_loc_y = 0
+    if snake_temp_loc_y == -1:
+        snake_temp_loc_y = 4
+    snake_loc_y.append(snake_temp_loc_y)
+    snake_loc_y.remove_at(0)
+
 def on_button_pressed_a():
+    a()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def b():
+    global snake_velocity_x, snake_velocity_y
+    if snake_velocity_x == 0:
+        snake_velocity_x = snake_velocity_y * -1
+        snake_velocity_y = 0
+    elif snake_velocity_y == 0:
+        snake_velocity_y = snake_velocity_x
+        snake_velocity_x = 0
+def update_snake_x():
+    global snake_temp_loc_x
+    snake_temp_loc_x = snake_loc_x[len(snake_loc_x) - 1] + snake_velocity_x
+    if snake_temp_loc_x == -1:
+        snake_temp_loc_x = 4
+    if snake_temp_loc_x == 5:
+        snake_temp_loc_x = 0
+    snake_loc_x.append(snake_temp_loc_x)
+    snake_loc_x.remove_at(0)
+def a():
     global snake_velocity_x, snake_velocity_y
     if snake_velocity_x == 0 and snake_velocity_y == -1:
         snake_velocity_x = -1
@@ -12,38 +48,25 @@ def on_button_pressed_a():
     elif snake_velocity_x == -1 and snake_velocity_y == 0:
         snake_velocity_x = 0
         snake_velocity_y = 1
-input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global snake_velocity_x, snake_velocity_y
-    if snake_velocity_x == 0:
-        snake_velocity_x = snake_velocity_y * -1
-        snake_velocity_y = 0
-    elif snake_velocity_y == 0:
-        snake_velocity_y = snake_velocity_x
-        snake_velocity_x = 0
+    b()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+snake_temp_loc_x = 0
+snake_velocity_x = 0
+snake_temp_loc_y = 0
+snake_loc_y: List[number] = []
+snake_loc_x: List[number] = []
 snake_velocity_y = 0
-snake_velocity_x = 0
-snake_loc_x = 2
-snake_loc_y = 2
-snake_velocity_x = 0
-snake_velocity_y = -1
+snake_velocity_y = 1
+snake_loc_x = [1, 1, 1]
+snake_loc_y = [2, 1, 0]
 
 def on_forever():
-    global snake_loc_x, snake_loc_y
     basic.clear_screen()
-    led.plot(snake_loc_x, snake_loc_y)
-    basic.pause(200)
-    snake_loc_x = snake_loc_x + snake_velocity_x
-    if snake_loc_x == -1:
-        snake_loc_x = 4
-    if snake_loc_x == 5:
-        snake_loc_x = 0
-    snake_loc_y = snake_loc_y + snake_velocity_y
-    if snake_loc_y == 5:
-        snake_loc_y = 0
-    if snake_loc_y == -1:
-        snake_loc_y = 4
+    draw_snake()
+    update_snake_x()
+    update_snake_y()
+    basic.pause(250)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,45 @@
+function draw_snake () {
+    snake_pxl_loc = 0
+    while (snake_pxl_loc <= snake_loc_x.length + -1) {
+        led.plot(snake_loc_x[snake_pxl_loc], snake_loc_y[snake_pxl_loc])
+        snake_pxl_loc += 1
+    }
+}
+function update_snake_y () {
+    snake_temp_loc_y = snake_loc_y[snake_loc_x.length - 1] + snake_velocity_y
+    if (snake_temp_loc_y == 5) {
+        snake_temp_loc_y = 0
+    }
+    if (snake_temp_loc_y == -1) {
+        snake_temp_loc_y = 4
+    }
+    snake_loc_y.push(snake_temp_loc_y)
+    snake_loc_y.removeAt(0)
+}
 input.onButtonPressed(Button.A, function () {
+    a()
+})
+function b () {
+    if (snake_velocity_x == 0) {
+        snake_velocity_x = snake_velocity_y * -1
+        snake_velocity_y = 0
+    } else if (snake_velocity_y == 0) {
+        snake_velocity_y = snake_velocity_x
+        snake_velocity_x = 0
+    }
+}
+function update_snake_x () {
+    snake_temp_loc_x = snake_loc_x[snake_loc_x.length - 1] + snake_velocity_x
+    if (snake_temp_loc_x == -1) {
+        snake_temp_loc_x = 4
+    }
+    if (snake_temp_loc_x == 5) {
+        snake_temp_loc_x = 0
+    }
+    snake_loc_x.push(snake_temp_loc_x)
+    snake_loc_x.removeAt(0)
+}
+function a () {
     if (snake_velocity_x == 0 && snake_velocity_y == -1) {
         snake_velocity_x = -1
         snake_velocity_y = 0
@@ -12,45 +53,24 @@ input.onButtonPressed(Button.A, function () {
         snake_velocity_x = 0
         snake_velocity_y = 1
     }
-})
-input.onButtonPressed(Button.B, function () {
-    if (snake_velocity_x == 0) {
-        snake_velocity_x = snake_velocity_y * -1
-        snake_velocity_y = 0
-    } else if (snake_velocity_y == 0) {
-        snake_velocity_y = snake_velocity_x
-        snake_velocity_x = 0
-    }
-})
-function apple_random_location () {
-    apple_loc_x = randint(0, 4)
-    apple_loc_y = randint(0, 4)
 }
-let apple_loc_y = 0
-let apple_loc_x = 0
-let snake_velocity_y = 0
+input.onButtonPressed(Button.B, function () {
+    b()
+})
+let snake_temp_loc_x = 0
 let snake_velocity_x = 0
-let snake_loc_x = 2
-let snake_loc_y = 2
-snake_velocity_x = 0
-snake_velocity_y = -1
+let snake_temp_loc_y = 0
+let snake_pxl_loc = 0
+let snake_loc_y: number[] = []
+let snake_loc_x: number[] = []
+let snake_velocity_y = 0
+snake_velocity_y = 1
+snake_loc_x = [1, 1, 1]
+snake_loc_y = [2, 1, 0]
 basic.forever(function () {
     basic.clearScreen()
-    led.plot(apple_loc_x, apple_loc_y)
-    led.plot(snake_loc_x, snake_loc_y)
-    basic.pause(200)
-    snake_loc_x = snake_loc_x + snake_velocity_x
-    if (snake_loc_x == -1) {
-        snake_loc_x = 4
-    }
-    if (snake_loc_x == 5) {
-        snake_loc_x = 0
-    }
-    snake_loc_y = snake_loc_y + snake_velocity_y
-    if (snake_loc_y == 5) {
-        snake_loc_y = 0
-    }
-    if (snake_loc_y == -1) {
-        snake_loc_y = 4
-    }
+    draw_snake()
+    update_snake_x()
+    update_snake_y()
+    basic.pause(250)
 })
