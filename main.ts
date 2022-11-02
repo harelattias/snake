2e6bf48a3118a32e12d93e7dc772420fea27b073
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-11-02 16:42:36 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,31 @@
-function draw_snake () {
-    snake_pxl_loc = 0
+function draw_snake() {
+    let snake_pxl_loc = 0
     while (snake_pxl_loc <= snake_loc_x.length + -1) {
         led.plot(snake_loc_x[snake_pxl_loc], snake_loc_y[snake_pxl_loc])
         snake_pxl_loc += 1
     }
 }
-function update_snake_y () {
-    snake_temp_loc_y = snake_loc_y[snake_loc_y.length - 1] + snake_velocity_y
+
+function update_snake_y() {
+    
+    snake_temp_loc_y = snake_loc_y[snake_loc_x.length - 1] + snake_velocity_y
     if (snake_temp_loc_y == 5) {
         snake_temp_loc_y = 0
     }
+    
     if (snake_temp_loc_y == -1) {
         snake_temp_loc_y = 4
     }
+    
     snake_loc_y.push(snake_temp_loc_y)
+    snake_loc_y.removeAt(0)
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     a()
 })
-function b () {
+function b() {
+    
     if (snake_velocity_x == 0) {
         snake_velocity_x = snake_velocity_y * -1
         snake_velocity_y = 0
@@ -26,21 +33,26 @@ function b () {
         snake_velocity_y = snake_velocity_x
         snake_velocity_x = 0
     }
+    
 }
-function draw_apple () {
-    led.plot(apple_loc_x, apple_loc_y)
-}
-function update_snake_x () {
+
+function update_snake_x() {
+    
     snake_temp_loc_x = snake_loc_x[snake_loc_x.length - 1] + snake_velocity_x
     if (snake_temp_loc_x == -1) {
         snake_temp_loc_x = 4
     }
+    
     if (snake_temp_loc_x == 5) {
         snake_temp_loc_x = 0
     }
+    
     snake_loc_x.push(snake_temp_loc_x)
+    snake_loc_x.removeAt(0)
 }
-function a () {
+
+function a() {
+    
     if (snake_velocity_x == 0 && snake_velocity_y == -1) {
         snake_velocity_x = -1
         snake_velocity_y = 0
@@ -54,42 +66,25 @@ function a () {
         snake_velocity_x = 0
         snake_velocity_y = 1
     }
+    
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     b()
 })
-function apple_rand_loc () {
-    apple_loc_y = randint(0, 4)
-    apple_loc_x = randint(0, 4)
-}
-function check_snake_eat_apple () {
-    if (snake_temp_loc_y == apple_loc_y && snake_temp_loc_x == apple_loc_x) {
-        apple_rand_loc()
-    } else {
-        snake_loc_x.removeAt(0)
-        snake_loc_y.removeAt(0)
-    }
-}
 let snake_temp_loc_x = 0
-let apple_loc_y = 0
-let apple_loc_x = 0
-let snake_temp_loc_y = 0
-let snake_pxl_loc = 0
-let snake_loc_y: number[] = []
-let snake_loc_x: number[] = []
 let snake_velocity_x = 0
+let snake_temp_loc_y = 0
+let snake_loc_y : number[] = []
+let snake_loc_x : number[] = []
 let snake_velocity_y = 0
-apple_rand_loc()
 snake_velocity_y = 1
-snake_velocity_x = 0
-snake_loc_x = [1]
-snake_loc_y = [2]
-basic.forever(function () {
+snake_loc_x = [1, 1, 1]
+snake_loc_y = [2, 1, 0]
+basic.forever(function on_forever() {
     basic.clearScreen()
-    draw_apple()
     draw_snake()
     update_snake_x()
     update_snake_y()
-    check_snake_eat_apple()
-    basic.pause(300)
+    basic.pause(250)
 })
